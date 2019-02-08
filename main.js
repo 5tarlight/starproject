a8ed780e25a3457f78ddd87918c1ae0e7acd2702
5tarlight <developerstarlight@gmail.com>
2019-02-08 23:24:58 +0900

    404

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const http = require('http')
 const som = require('./lib/socket/main')
 const cookie = require('cookie-parser')
 const session = require('express-session')
+const eeh = require('express-error-handler')
 
 const app = express()
 
@@ -14,6 +15,16 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }))
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const eh = eeh({
+  static: {
+    '404': './public/404.html'
+  }
+})
+
+app.use(eeh.httpError(404))
+app.use(eh)
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/lib/public/index.html');
