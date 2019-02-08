a71e6c16a8e14112b16c5520cfc90f572f57446d
5tarlight <developerstarlight@gmail.com>
2019-02-08 23:18:55 +0900

    session and cookie

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const http = require('http')
 const som = require('./lib/socket/main')
+const cookie = require('cookie-parser')
+const session = require('express-session')
 
 const app = express()
 
 app.set('port', 80)
+
+app.use(cookie())
+app.use(session({
+  secret: 'my key',
+  resave: true,
+  saveUninitialized: true
+}))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/lib/public/index.html');
