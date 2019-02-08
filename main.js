e8a5c4e1c75dd424a88dc29902737b2b7fd427fb
5tarlight <developerstarlight@gmail.com>
2019-02-08 17:41:04 +0900

    socket.io test

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const app = express()
 
 app.set('port', 80)
 
-http.createServer(app).listen(app.get('port'), () => {
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/lib/public/index.html');
+});
+
+const server = http.createServer(app).listen(app.get('port'), () => {
   console.log("Server Ready")
+})
+
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+  console.log('client connected')
+  
+  socket.on('msg', (data) => {
+    console.log('message : ' + data.msg)
+  })
 })
